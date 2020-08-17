0c04077421d2570e45f0af92fa3376b4907f1770
berkangzgn <bgerkanezgin.bg2014@gmail.com>
2020-08-17 19:05:08 +0300

    Update, destroy and edit

---
+++ app/models/article.rb
@@ -0,0 +1,22 @@
+class Article < ApplicationRecord
+
+  validates :title, presence: true,
+            length: { minimum: 5 }
+  end
+
+
+
+
+
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :text))
+    @article.save
+    redirect_to @article
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
+
